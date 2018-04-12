d9e29e79e314967f1214fcdfc17395d2660af300
= <=>
2018-04-13 01:45:03 +0900

    Gain : 304

--- Programming_Assignment_2/project_dt/dt.py
+++ Programming_Assignment_2/project_dt/dt.py
@@ -40,7 +40,7 @@ def generate_decision_tree (attribute_list, data_set) :
 		count = 0
 		new_level_nodes = [] # [level [node]]
 		for i in tree[level] :
-			label = get_label(i[0], i[2], attribute_list)
+			label = get_label(i[0], i[2], attribute_list, tree)
 
 			if label == None : # no more label or homogenous
 				count = count + 1
@@ -52,6 +52,7 @@ def generate_decision_tree (attribute_list, data_set) :
 		if count == len(tree[level]) :
 			break
 		level = level + 1
+
 
 	# if tree node has no data -> remove
 	# if tree node homogenous -> child label location = class
@@ -65,8 +66,8 @@ def generate_decision_tree (attribute_list, data_set) :
 			if len(j[1]) <= 0 : # if child node
 				majority_class = get_majority(j[2], list(attribute_list.values())[-1])
 				if majority_class == None : # no data
-					parent = get_parent(j[0], tree)
-					parent[1] = get_majority(parent[2], list(attribute_list.values())[-1])
+				#	parent_idx, parent_pos = get_parent_index(j[0], reduced_tree)
+				#	reduced_tree[parent_idx][parent_pos][1] = get_majority(reduced_tree[parent_idx][parent_pos][2], list(attribute_list.values())[-1])
 					reduced_tree[index].remove(j)
 					continue
 				reduced_tree[index][position][1].append(majority_class)
@@ -74,8 +75,7 @@ def generate_decision_tree (attribute_list, data_set) :
 		index = index - 1
 	return reduced_tree
 
-
-def get_label (used_label, data_set, attribute_list) :
+def get_label (used_label, data_set, attribute_list, tree) :
 	label_candidate = list(set(list(attribute_list.keys())[:-1])-set(used_label))
 
 	# list for saving gini values
@@ -89,14 +89,24 @@ def get_label (used_label, data_set, attribute_list) :
 	if len(list(gini.keys())) <= 0 :
 		return None
 
+	gain = gini.copy()
+	gain_ratio = gain.copy()
+
 	# calculate gini each labels
 	gini_val = get_gini(list(attribute_list.keys())[-1], list(attribute_list.values())[-1], data_set)
+	info_val = get_info(list(attribute_list.keys())[-1], list(attribute_list.values())[-1], data_set)
+
 	for i in label_candidate :
 		# append data by attribute by i (label)
 		data_set_by_label_attribute = [[] for _ in attribute_list[i]]
 		gini[i] = get_gini_impurity(gini_val, i, attribute_list[i], list(attribute_list.keys()).index(i), list(attribute_list.keys())[-1], list(attribute_list.values())[-1], data_set)
-
-	print("gini", gini)
+		gain[i] = get_gain(info_val, i, attribute_list[i], list(attribute_list.keys()).index(i), list(attribute_list.keys())[-1], list(attribute_list.values())[-1], data_set)
+		splitted = get_splitted_info(i, attribute_list[i], list(attribute_list.keys()).index(i), data_set)
+		if splitted != 0 :
+			gain_ratio[i] = gain[i] / splitted
+		else :
+			gain_ratio[i] = gain[i]
+	"""
 	# find homogenous or return min label
 	if max(gini.values()) == 1 :
 		return None
@@ -105,6 +115,76 @@ def get_label (used_label, data_set, attribute_list) :
 	else :
 		new_label = label_candidate[list(gini.values()).index(max(list(gini.values())))]
 		return new_label
+	"""
+
+	# find homogenous or return min label
+	if max(gain_ratio.values()) == 1 :
+		return None
+	elif list(gain_ratio.values()) == gini_compare :
+		return None
+	else :
+		new_label = label_candidate[list(gain_ratio.values()).index(max(list(gain_ratio.values())))]
+		return new_label
+
+def get_info (class_label, class_attribute, data_set) :
+	# count by class attribute
+	count = [0 for _ in class_attribute]
+	for i in data_set :
+		count[class_attribute.index(i[-1])] += 1
+
+	# if no data, return None
+	total = len(data_set)
+	if total <= 0 :
+		return 0
+
+	# calculate info
+	info = 0
+	for i in count :
+		if i != 0 :
+			info -= ((i/total)*log(i/total, 2))
+	return info
+
+def get_average_info (label, label_attribute, label_index, class_label, class_attribute, data_set) :
+	count_by_label_attribute = [0 for _ in label_attribute]
+	data_set_by_label_attribute = [[] for _ in label_attribute]
+
+	# count data by label attribute
+	for i in data_set :
+		count_by_label_attribute[label_attribute.index(i[label_index])] += 1
+		data_set_by_label_attribute[label_attribute.index(i[label_index])].append(i)
+
+	if sum(count_by_label_attribute) <= 0 : # no data
+		return 0
+
+	info_avg = 0
+	index = 0
+	for i in count_by_label_attribute :
+		info_avg += (i/sum(count_by_label_attribute) * get_info(class_label, class_attribute, data_set_by_label_attribute[index]))
+		index = index + 1
+
+	return info_avg
+
+def get_gain (info, label, label_attribute, label_index, class_label, class_attribute, data_set) :
+	return info - get_average_info(label, label_attribute, label_index, class_label, class_attribute, data_set)
+
+def get_splitted_info (label, label_attribute, label_index, data_set) :
+	count_by_label_attribute = [0 for _ in label_attribute]
+
+	# count data by label attribute
+	for i in data_set :
+		count_by_label_attribute[label_attribute.index(i[label_index])] += 1
+
+	# if no data, return None
+	total = len(data_set)
+	if total <= 0 :
+		return 0
+
+	# calculate info
+	info = 0
+	for i in count_by_label_attribute :
+		if i != 0 :
+			info -= ((i/total)*log(i/total, 2))
+	return info
 
 def get_gini (class_label, class_attribute, data_set) :
 	# count by class attribute
@@ -118,7 +198,7 @@ def get_gini (class_label, class_attribute, data_set) :
 		return 0
 
 	# calculate gini
-	gini = 1
+	gini = 0
 	for i in count :
 		gini -= ((i/total) * (i/total))
 
@@ -183,7 +263,7 @@ def get_majority (data_set, class_attribute) :
 
 	return class_attribute[count.index(max(count))]
 
-def get_parent(label, tree) :
+def get_parent (label, tree) :
 	label_copy = label.copy()
 	del label_copy[list(label_copy.keys())[0]]
 
@@ -194,6 +274,21 @@ def get_parent(label, tree) :
 
 	return get_parent(tree, label_copy)
 
+def get_parent_index (label, tree) :
+	label_copy = label.copy()
+	del label_copy[list(label_copy.keys())[0]]
+
+	index = 0
+	position = 0
+	for i in tree :
+		position = 0
+		for j in i :
+			if j[0] == label:
+				return index, position
+			position = position + 1
+		index = index + 1
+
+	return get_parent_index(tree, label_copy)
 
 def get_class (data, attribute_list, tree) :
 	# initial search settings
@@ -204,12 +299,13 @@ def get_class (data, attribute_list, tree) :
 		for j in i : # each node in level
 			if label.items() <= j[0].items() : # if all label in current node label
 				if len(j[1]) <= 0 : # no next label
-					parent_label = label.copy()
-					current_class = None
-					while current_class == None :
-						parent = get_parent(parent_label, tree)
-						current_class = get_majority(parent[2], list(attribute_list.values())[-1])
-						return current_class
+					#parent_label = label.copy()
+					#current_class = None
+					#while current_class == None :
+					#	parent = get_parent(parent_label, tree)
+					#	current_class = get_majority(parent[2], list(attribute_list.values())[-1])
+					#	return current_class
+					return get_majority(j[2], list(attribute_list.values())[-1])
 				elif j[1][0] in list(attribute_list.values())[-1] : # if child label == class (leaf node)
 					return j[1][0]
 				else : # get next level
